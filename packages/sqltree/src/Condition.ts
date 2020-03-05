c47711d346461c99a95565e998f1af5244c92dbf
Jan Prochazka <jan.prochazka@gmail.com>
2020-03-05 12:24:07 +0100

    deleted obsolete files

--- packages/sqltree/src/Condition.ts
+++
@@ -1,9 +0,0 @@
-// import { SqlDumper } from "@dbgate/types";
-
-// export abstract class Condition {
-//     abstract dumpSql(dumper: SqlDumper) ;
-// }
-
-// export abstract class UnaryCondition extends Condition {
-//     // expr: Expresssion;
-// }
--- packages/sqltree/src/Expression.ts
+++
@@ -1,4 +0,0 @@
-
-// export abstract class Expression {
-
-// }
