03b656a25b164f6dbec1d9bcf9eeb445027539e4
Jan Prochazka <jan.prochazka@gmail.com>
2020-05-17 07:47:42 +0200

    pg stream query support

--- packages/api/src/utility/driverConnect.js
+++ packages/api/src/utility/driverConnect.js
@@ -1,11 +1,13 @@
 const mssql = require('mssql');
 const mysql = require('mysql');
 const pg = require('pg');
+const pgQueryStream = require('pg-query-stream');
 
 const nativeModules = {
   mssql,
   mysql,
   pg,
+  pgQueryStream,
 };
 
 function driverConnect(driver, connection) {
--- packages/engines/postgres/index.js
+++ packages/engines/postgres/index.js
@@ -1,10 +1,11 @@
+const _ = require('lodash');
 const PostgreAnalyser = require('./PostgreAnalyser');
 const PostgreDumper = require('./PostgreDumper');
 
 /** @type {import('@dbgate/types').SqlDialect} */
 const dialect = {
   rangeSelect: true,
-  stringEscapeChar: "\\",
+  stringEscapeChar: '\\',
   quoteIdentifier(s) {
     return '"' + s + '"';
   },
@@ -13,7 +14,13 @@ const dialect = {
 /** @type {import('@dbgate/types').EngineDriver} */
 const driver = {
   async connect(nativeModules, { server, port, user, password, database }) {
-    const client = new nativeModules.pg.Client({ host: server, port, user, password, database: database || 'postgres' });
+    const client = new nativeModules.pg.Client({
+      host: server,
+      port,
+      user,
+      password,
+      database: database || 'postgres',
+    });
     await client.connect();
     client._nativeModules = nativeModules;
     return client;
@@ -21,6 +28,67 @@ const driver = {
   async query(client, sql) {
     const res = await client.query(sql);
     return { rows: res.rows, columns: res.fields };
+  },
+  async stream(client, sql, options) {
+    const query = new client._nativeModules.pgQueryStream(sql);
+    const stream = client.query(query);
+
+    // const handleInfo = (info) => {
+    //   const { message, lineNumber, procName } = info;
+    //   options.info({
+    //     message,
+    //     line: lineNumber,
+    //     procedure: procName,
+    //     time: new Date(),
+    //     severity: 'info',
+    //   });
+    // };
+
+    let wasHeader = false;
+
+    const handleEnd = (result) => {
+      // console.log('RESULT', result);
+      options.done(result);
+    };
+
+    const handleReadable = () => {
+      let row = stream.read();
+      if (!wasHeader && row) {
+        options.recordset(_.keys(row).map((columnName) => ({ columnName })));
+        wasHeader = true;
+      }
+
+      while (row) {
+        options.row(row);
+        row = stream.read();
+      }
+    };
+
+    // const handleFields = (columns) => {
+    //   // console.log('FIELDS', columns[0].name);
+    //   options.recordset(columns);
+    //   // options.recordset(extractColumns(columns));
+    // };
+
+    const handleError = (error) => {
+      console.log('ERROR', error);
+      const { message, lineNumber, procName } = error;
+      options.info({
+        message,
+        line: lineNumber,
+        procedure: procName,
+        time: new Date(),
+        severity: 'error',
+      });
+    };
+
+    stream.on('error', handleError);
+    stream.on('readable', handleReadable);
+    // stream.on('result', handleRow)
+    // stream.on('data', handleRow)
+    stream.on('end', handleEnd);
+
+    return stream;
   },
   async getVersion(client) {
     const { rows } = await this.query(client, 'SELECT version()');
