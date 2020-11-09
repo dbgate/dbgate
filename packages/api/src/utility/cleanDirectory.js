1379ba50268147dfbca6db372f6a0878d56d31dd
Jan Prochazka <jan.prochazka@gmail.com>
2020-11-09 08:24:49 +0100

    build fix

--- packages/api/src/utility/cleanDirectory.js
+++ packages/api/src/utility/cleanDirectory.js
@@ -1,11 +1,11 @@
-const findRemoveSync = require('find-remove');
+// const findRemoveSync = require('find-remove');
 
 function cleanDirectory(directory) {
-  findRemoveSync(directory, {
-    age: { seconds: 3600 },
-    files: '*.*',
-    dir: '*',
-  });
+//   findRemoveSync(directory, {
+//     age: { seconds: 3600 },
+//     files: '*.*',
+//     dir: '*',
+//   });
 }
 
 module.exports = cleanDirectory;
