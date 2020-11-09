7819cc954126759792c5637130563d6710f0424f
Jan Prochazka <jan.prochazka@gmail.com>
2020-11-09 18:03:18 +0100

    clean temp files

--- packages/api/src/utility/cleanDirectory.js
+++ packages/api/src/utility/cleanDirectory.js
@@ -1,11 +1,24 @@
-// const findRemoveSync = require('find-remove');
+const fs = require('fs-extra');
+const path = require('path');
+const ageSeconds = 3600;
 
-function cleanDirectory(directory) {
-//   findRemoveSync(directory, {
-//     age: { seconds: 3600 },
-//     files: '*.*',
-//     dir: '*',
-//   });
+async function cleanDirectory(directory) {
+  const files = await fs.readdir(directory);
+  const now = new Date().getTime();
+
+  for (const file of files) {
+    const full = path.join(directory, file);
+    const stat = await fs.stat(full);
+    const mtime = stat.mtime.getTime();
+    const expirationTime = mtime + ageSeconds * 1000;
+    if (now > expirationTime) {
+      if (stat.isDirectory()) {
+        await fs.rmdir(full, { recursive: true });
+      } else {
+        await fs.unlink(full);
+      }
+    }
+  }
 }
 
 module.exports = cleanDirectory;
