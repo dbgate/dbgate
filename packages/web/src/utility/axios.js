d3cfc44fd984061d9881f7adf68aec50c1ce2a75
Jan Prochazka <jan.prochazka@gmail.com>
2020-11-22 18:00:01 +0100

    disable axios cache

--- packages/web/src/utility/axios.js
+++ packages/web/src/utility/axios.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import resolveApi from './resolveApi';
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: resolveApi(),
 });
+
+axiosInstance.defaults.headers = {
+  'Cache-Control': 'no-cache',
+  Pragma: 'no-cache',
+  Expires: '0',
+};
+
+export default axiosInstance;
