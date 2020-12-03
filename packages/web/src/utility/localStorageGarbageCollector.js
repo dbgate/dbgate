9a42d1d6bd8f5fa78c58e1477e77994392a88f7f
Jan Prochazka <jan.prochazka@gmail.com>
2020-12-03 13:22:54 +0100

    added local forage

--- packages/web/src/utility/localStorageGarbageCollector.js
+++ packages/web/src/utility/localStorageGarbageCollector.js
@@ -1,8 +1,8 @@
 import moment from 'moment';
+import localforage from 'localforage';
 
-export default function localStorageGarbageCollector() {
+export default async function localStorageGarbageCollector() {
   const openedTabsJson = localStorage.getItem('openedTabs');
-  const savedSqlFilesJson = localStorage.getItem('savedSqlFiles');
   let openedTabs = openedTabsJson ? JSON.parse(openedTabsJson) : [];
 
   const closeLimit = moment().add(-7, 'day').valueOf();
@@ -10,16 +10,26 @@ export default function localStorageGarbageCollector() {
   openedTabs = openedTabs.filter((x) => !x.closedTime || x.closedTime > closeLimit);
   localStorage.setItem('openedTabs', JSON.stringify(openedTabs));
 
-  const savedSqlFiles = savedSqlFilesJson ? JSON.parse(savedSqlFilesJson) : [];
   const toRemove = [];
   for (const key in localStorage) {
     if (!key.startsWith('tabdata_')) continue;
-    if (savedSqlFiles.find((x) => x.storageKey == key)) continue;
     if (openedTabs.find((x) => key.endsWith('_' + x.tabid))) continue;
     toRemove.push(key);
   }
 
   for (const key of toRemove) {
     localStorage.removeItem(key);
+  }
+
+  const keysForage = await localforage.keys();
+  const toRemoveForage = [];
+  for (const key in keysForage) {
+    if (!key.startsWith('tabdata_')) continue;
+    if (openedTabs.find((x) => key.endsWith('_' + x.tabid))) continue;
+    toRemoveForage.push(key);
+  }
+
+  for (const key of toRemoveForage) {
+    await localforage.removeItem(key);
   }
 }
