d8fcbc8c170fe79d07fec39fab51c95dbeca6913
Jan Prochazka <jan.prochazka@gmail.com>
2021-03-27 10:14:00 +0100

    database context menu

--- packages/web/src/appobj/DatabaseAppObject.svelte
+++ packages/web/src/appobj/DatabaseAppObject.svelte
@@ -4,12 +4,62 @@
 
 <script lang="ts">
   import _ from 'lodash';
-
-  import { currentDatabase } from '../stores';
-
+  import ImportExportModal from '../modals/ImportExportModal.svelte';
+  import { showModal } from '../modals/modalTools';
+  import { getDefaultFileFormat } from '../plugins/fileformats';
+  import { currentDatabase, extensions } from '../stores';
+  import openNewTab from '../utility/openNewTab';
   import AppObjectCore from './AppObjectCore.svelte';
-
   export let data;
+
+  const handleNewQuery = () => {
+    const { connection, name } = data;
+    const tooltip = `${connection.displayName || connection.server}\n${name}`;
+    openNewTab({
+      title: 'Query #',
+      icon: 'img sql-file',
+      tooltip,
+      tabComponent: 'QueryTab',
+      props: {
+        conid: connection._id,
+        database: name,
+      },
+    });
+  };
+
+  const handleImport = () => {
+    const { connection, name } = data;
+
+    showModal(ImportExportModal, {
+      initialValues: {
+        sourceStorageType: getDefaultFileFormat($extensions).storageType,
+        targetStorageType: 'database',
+        targetConnectionId: connection._id,
+        targetDatabaseName: name,
+      },
+    });
+  };
+
+  const handleExport = () => {
+    const { connection, name } = data;
+
+    showModal(ImportExportModal, {
+      initialValues: {
+        targetStorageType: getDefaultFileFormat($extensions).storageType,
+        sourceStorageType: 'database',
+        sourceConnectionId: connection._id,
+        sourceDatabaseName: name,
+      },
+    });
+  };
+
+  function createMenu() {
+    return [
+      { onClick: handleNewQuery, text: 'New query' },
+      { onClick: handleImport, text: 'Import' },
+      { onClick: handleExport, text: 'Export' },
+    ];
+  }
 </script>
 
 <AppObjectCore
@@ -20,4 +70,5 @@
   isBold={_.get($currentDatabase, 'connection._id') == _.get(data.connection, '_id') &&
     _.get($currentDatabase, 'name') == data.name}
   on:click={() => ($currentDatabase = data)}
+  menu={createMenu}
 />
