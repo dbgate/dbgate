78a59ae8dc42cb2677ca241474fa7d467f5da820
Jan Prochazka <jan.prochazka@gmail.com>
2021-05-30 11:00:32 +0200

    filterparser tests added to test run

--- packages/filterparser/src/parserFilter.test.ts
+++ packages/filterparser/src/parserFilter.test.ts
@@ -3,5 +3,9 @@ import { parseFilter } from './parseFilter';
 test('parse string', () => {
   const ast = parseFilter('"123"', 'string');
   console.log(JSON.stringify(ast));
-  expect(ast).toBe(3);
+  expect(ast).toEqual({
+    conditionType: 'like',
+    left: { exprType: 'placeholder' },
+    right: { exprType: 'value', value: '%123%' },
+  });
 });
