2e03056a157913a5a0886890037e2ae2279bd104
Jan Prochazka <jan.prochazka@gmail.com>
2021-09-06 18:25:34 +0200

    tests

--- integration-tests/engines.js
+++ integration-tests/engines.js
@@ -116,11 +116,11 @@ const engines = [
 
 const filterLocal = [
   // filter local testing
-  '-MySQL',
-  '-PostgreSQL',
+  'MySQL',
+  'PostgreSQL',
   'SQL Server',
-  '-SQLite',
-  '-CockroachDB',
+  'SQLite',
+  'CockroachDB',
 ];
 
 module.exports = process.env.CITEST
