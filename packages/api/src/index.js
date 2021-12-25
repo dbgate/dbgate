6dcbb5e308596c6afa7cdc6d909e333887eeda20
Jan Prochazka <jan.prochazka@gmail.com>
2021-12-25 09:27:29 +0100

    fix

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -8,13 +8,11 @@ if (processArgs.startProcess) {
   const proc = require('./proc');
   const module = proc[processArgs.startProcess];
   module.start();
+} else if (!module['parent'] && !processArgs.checkParent) {
+  const main = require('./main');
+
+  main.start();
 }
-
-// else if (!module['parent'] && !processArgs.checkParent) {
-//   const main = require('./main');
-
-//   main.start();
-// }
 
 module.exports = {
   ...shell,
