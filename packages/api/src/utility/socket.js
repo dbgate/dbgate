1c977bb7ae84eba6aea3fd5050f2c58e18b3f1b0
Jan Prochazka <jan.prochazka@gmail.com>
2022-02-26 09:58:15 +0100

    smaller SSE ping interval

--- packages/api/src/utility/socket.js
+++ packages/api/src/utility/socket.js
@@ -5,7 +5,7 @@ let init = '';
 module.exports = {
   setSseResponse(value) {
     sseResponse = value;
-    setInterval(() => this.emit('ping'), 30 * 1000);
+    setInterval(() => this.emit('ping'), 29 * 1000);
   },
   setElectronSender(value) {
     electronSender = value;
