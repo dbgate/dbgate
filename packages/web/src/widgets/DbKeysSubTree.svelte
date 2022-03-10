730fd38b9efd0cee15733e9ac2778e9b60aca203
Jan Prochazka <jan.prochazka@gmail.com>
2022-03-10 17:21:04 +0100

    redis: dont show all keys in tree (show more...)

--- packages/web/src/widgets/DbKeysSubTree.svelte
+++ packages/web/src/widgets/DbKeysSubTree.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import AppObjectCore from '../appobj/AppObjectCore.svelte';
+
+  const SHOW_INCREMENT = 500;
+
   import { useDatabaseKeys } from '../utility/metadataLoaders';
   import DbKeysTreeNode from './DbKeysTreeNode.svelte';
 
@@ -8,9 +12,23 @@
   export let root;
   export let indentLevel = 0;
 
+  let maxShowCount = SHOW_INCREMENT;
+
   $: items = useDatabaseKeys({ conid, database, root });
 </script>
 
-{#each $items || [] as item}
+{#each ($items || []).slice(0, maxShowCount) as item}
   <DbKeysTreeNode {conid} {database} {root} {item} {indentLevel} />
 {/each}
+
+{#if ($items || []).length > maxShowCount}
+  <AppObjectCore
+    {indentLevel}
+    title="Show more..."
+    icon="icon dots-horizontal"
+    expandIcon="icon invisible-box"
+    on:click={() => {
+      maxShowCount += SHOW_INCREMENT;
+    }}
+  />
+{/if}
