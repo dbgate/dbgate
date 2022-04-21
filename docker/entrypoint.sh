e5bbf5eca35dabf7bcc934b863121175545d4f0a
Jan Prochazka <jan.prochazka@gmail.com>
2022-04-21 18:59:07 +0200

    dockerhost name support

---
+++ docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+HOST_DOMAIN="dockerhost"
+ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
+if [ $? != 0 ]
+then
+  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
+  echo "$HOST_IP $HOST_DOMAIN" >> /etc/hosts
+fi
+
+node bundle.js
