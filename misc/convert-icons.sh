d79c9e159a04a4944dc317c939137520d6c2197b
Jan Prochazka <jan.prochazka@gmail.com>
2022-04-28 08:16:27 +0200

    icon

--- misc/convert-icons.sh
+++ misc/convert-icons.sh
@@ -29,33 +29,42 @@
 # magick icon.svg -resize 1024x1024 icon.png
 # magick icon.svg -resize 1024x1024 -transparent white -background transparent icon.png
 
-magick -size 1000x1000 xc:transparent \
-    -fill '#096dd9' -stroke '#002766' -strokewidth 10 \
-    -draw "rectangle 150,150 950,850" \
-    -draw "arc 150,750 950,950  0,180" \
-    -strokewidth 0 -stroke '#096dd9' \
-    -draw "rectangle 155,840 945 860" \
-    -strokewidth 10 -stroke '#002766' \
-    -fill '#1890ff' \
-    -draw "arc 150,50 950,250  0,360" \
-    -strokewidth 10 -stroke '#0050b3' -fill transparent \
-    -draw "arc 150,225 950,425  0,180" \
-    -draw "arc 150,400 950,600  0,180" \
-    -draw "arc 150,575 950,775  0,180" \
-    -draw "arc 150,750 950,950  0,180" \
-    -fill '#fafafa' -stroke '#8c8c8c' -strokewidth 3 \
-    -pointsize 700 -font Arial \
-    -gravity center \
-    -draw 'text 50,100 "G"' \
+STROKE_WIDTH=30
+LEFT=150
+RIGHT=850
+
+
+
+magick  \
+    \( \
+        -size 1000x1000 -define gradient:direction=east 'gradient:#0050b3-#1890ff' \
+        \( +clone -fill Black -colorize 100 \
+        -fill White -stroke White -draw "arc $LEFT,750 $RIGHT,950 0,360" -draw "rectangle $LEFT,150 $RIGHT,850" \
+        \) \
+        -alpha off \
+        -compose CopyOpacity -composite \
+    \) \
     \( \
         -size 1000x1000 -define gradient:direction=east 'gradient:#096dd9-#40a9ff' \
         \( +clone -fill Black -colorize 100 \
-        -fill White -draw "arc 150,50 950,250  0,360" \
+        -fill White -draw "arc $LEFT,50 $RIGHT,250 0,360" \
         \) \
         -alpha off \
         -compose CopyOpacity -composite \
     \) \
     -compose Over -composite \
+    -strokewidth $STROKE_WIDTH -stroke '#0050b3' -fill transparent \
+    -draw "arc $LEFT,225 $RIGHT,425  0,180" \
+    -draw "arc $LEFT,400 $RIGHT,600  0,180" \
+    -draw "arc $LEFT,575 $RIGHT,775  0,180" \
+    -draw "arc $LEFT,750 $RIGHT,950  0,180" \
+    -draw "arc $LEFT,50 $RIGHT,250  0,360" \
+    -draw "line $LEFT,150 $LEFT,850" \
+    -draw "line $RIGHT,150 $RIGHT,850" \
+    -fill '#fafafa' -stroke '#8c8c8c' -strokewidth 3 \
+    -pointsize 800 -font './Mcbungus-Regular.ttf' \
+    -gravity center \
+    -draw 'text 0,100 "G"' \
     icon.png
 
 
