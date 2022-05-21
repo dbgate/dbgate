d6d7ad99df5c1cdafa34444cf25aac3728d8e9d2
Jan Prochazka <jan.prochazka@gmail.com>
2022-05-21 08:20:23 +0200

    test fix

--- integration-tests/__tests__/table-analyse.spec.js
+++ integration-tests/__tests__/table-analyse.spec.js
@@ -14,7 +14,7 @@ const txMatch = (tname, vcolname, nextcol) =>
       expect.objectContaining({
         columnName: 'id',
         notNull: true,
-        dataType: expect.stringContaining('INT'),
+        dataType: expect.stringMatching(/int/i),
       }),
       expect.objectContaining({
         columnName: vcolname,
