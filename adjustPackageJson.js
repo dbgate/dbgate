bc4c146389e87ed2727e6b217b59903fb29b55e0
Jan Prochazka <jan.prochazka@gmail.com>
2022-08-07 10:02:11 +0200

    remove msnodesqlv8 from mac+linux build

---
+++ adjustPackageJson.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+function adjustFile(file) {
+  const json = JSON.parse(fs.readFileSync(file, { encoding: 'utf-8' }));
+  if (process.platform != 'win32') {
+    delete json.optionalDependencies.msnodesqlv8;
+  }
+  fs.writeFileSync(file, JSON.stringify(json, null, 2), 'utf-8');
+}
+
+adjustFile('package.json');
+adjustFile('app/package.json');
