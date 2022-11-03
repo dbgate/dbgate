a3b1ac550617a430065084321aa642011b21aa69
Gernot Pansy <gernot.pansy@ut11.net>
2022-11-03 11:19:01 +0100

    Set default database as auth source for mongodb connection
    This change may break some configurations, but it seems that the default database is used for authentication also on other drivers.
    On the other hand, without that change it's not possible to connect to a mongodb where the authdb isn't the "admin" db.

--- plugins/dbgate-plugin-mongo/src/backend/driver.js
+++ plugins/dbgate-plugin-mongo/src/backend/driver.js
@@ -62,6 +62,9 @@ const driver = {
     const options = {
       useUnifiedTopology: true,
     };
+    if (database) {
+      options.authSource = database;
+    }
     if (ssl) {
       options.tls = true;
       options.tlsCAFile = ssl.ca;
