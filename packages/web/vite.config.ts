99c33b6b4b6d211d819a8c24f83c8ce6544e3404
Jan Prochazka <jan.prochazka@gmail.com>
2022-11-05 09:22:26 +0100

    vite refactor

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [
+    svelte(),
+    viteCommonjs({
+      include: ['mainMenuDefinition'],
+    }),
+  ],
+});
