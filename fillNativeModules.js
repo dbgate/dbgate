a7a8ea053bd6a2da0bf953d3244864a0fec5efe5
Jan Prochazka <jan.prochazka@gmail.com>
2022-11-20 17:03:55 +0100

    fill native modules - oracledb

--- fillNativeModules.js
+++ fillNativeModules.js
@@ -5,9 +5,12 @@ let fillContent = '';
 if (process.platform == 'win32') {
   fillContent += `content.msnodesqlv8 = () => require('msnodesqlv8');`;
 }
+if (process.arch != 'arm64') {
+  fillContent += `content.oracledb = () => require('oracledb');`;
+}
 fillContent += `content['better-sqlite3'] = () => require('better-sqlite3');`;
 
-const getContent = (empty) => `
+const getContent = empty => `
 // this file is generated automatically by script fillNativeModules.js, do not edit it manually
 const content = {};
 
