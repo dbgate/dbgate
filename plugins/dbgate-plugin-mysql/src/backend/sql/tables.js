3c63738809ff54bb90f95eae1c3f39c57c37388c
Jan Prochazka <jan.prochazka@gmail.com>
2022-12-15 17:38:02 +0100

    fixed missing versioned tables #433

--- plugins/dbgate-plugin-mysql/src/backend/sql/tables.js
+++ plugins/dbgate-plugin-mysql/src/backend/sql/tables.js
@@ -4,5 +4,5 @@ select
 	TABLE_ROWS as tableRowCount,
 	case when ENGINE='InnoDB' then CREATE_TIME else coalesce(UPDATE_TIME, CREATE_TIME) end as modifyDate 
 from information_schema.tables 
-where TABLE_SCHEMA = '#DATABASE#' and TABLE_TYPE='BASE TABLE' and TABLE_NAME =OBJECT_ID_CONDITION;
+where TABLE_SCHEMA = '#DATABASE#' and (TABLE_TYPE='BASE TABLE' or TABLE_TYPE='SYSTEM VERSIONED') and TABLE_NAME =OBJECT_ID_CONDITION;
 `;
