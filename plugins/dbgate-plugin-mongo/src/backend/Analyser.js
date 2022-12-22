b0d78250e1bbce775af20cef8583c36fba138317
Jan Prochazka <jan.prochazka@gmail.com>
2022-12-22 17:35:46 +0100

    row count info added to mongoDB

--- plugins/dbgate-plugin-mongo/src/backend/Analyser.js
+++ plugins/dbgate-plugin-mongo/src/backend/Analyser.js
@@ -8,9 +8,12 @@ class Analyser extends DatabaseAnalyser {
   async _runAnalysis() {
     const collections = await this.pool.__getDatabase().listCollections().toArray();
 
+    const stats = await Promise.all(collections.map((x) => this.pool.__getDatabase().collection(x.name).stats()));
+
     const res = this.mergeAnalyseResult({
-      collections: collections.map((x) => ({
+      collections: collections.map((x, index) => ({
         pureName: x.name,
+        tableRowCount: stats[index].count,
       })),
     });
     // console.log('MERGED', res);
