6c4b56a28b38c5752eb9543555b9555005f98f0d
Jan Prochazka <jan.prochazka@gmail.com>
2023-01-01 19:50:19 +0100

    fixed loading materialized views in oracle

--- plugins/dbgate-plugin-oracle/src/backend/sql/matviews.js
+++ plugins/dbgate-plugin-oracle/src/backend/sql/matviews.js
@@ -1,15 +1,19 @@
 module.exports = `
-select owner as schema_name,
+SELECT owner as schema_name,
        mview_name pure_name,
        container_name,
-       query as definition,
-         ora_hash(query, 'MD5') as "hash_code"
-       --refresh_mode,
-       --refresh_method,
-       --build_mode,
-       --last_refresh_date,
-       --ompile_state
-from all_mviews
+       '' || trim(
+         extractvalue(
+                  dbms_xmlgen.getxmltype('SELECT query
+                                          FROM all_mviews
+                                          WHERE mview_name=''' ||
+                                                MVIEW_NAME || ''' AND
+                                                owner = ''' ||
+                                                owner || ''''
+                                         ),
+                  '//text()'
+                )) definition
+FROM all_mviews
 where mview_name=OBJECT_ID_CONDITION
 order by owner, mview_name
 `;
