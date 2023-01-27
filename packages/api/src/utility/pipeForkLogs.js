2dd8749bc6652247f099fe2be136bd670578872e
Jan Prochazka <jan.prochazka@gmail.com>
2023-01-27 15:33:11 +0100

    simplified logging

--- packages/api/src/utility/pipeForkLogs.js
+++ packages/api/src/utility/pipeForkLogs.js
@@ -2,19 +2,10 @@ const byline = require('byline');
 const { safeJsonParse, getLogger } = require('dbgate-tools');
 const logger = getLogger();
 
-const levelNames = {
-  10: 'trace',
-  20: 'debug',
-  30: 'info',
-  40: 'warn',
-  50: 'error',
-  60: 'fatal',
-};
-
 const logDispatcher = method => data => {
   const json = safeJsonParse(data.toString());
-  if (json && json.level && levelNames[json.level]) {
-    logger[levelNames[json.level]](json || data.toString());
+  if (json && json.level) {
+    logger.log(json);
   } else {
     logger[method](json || data.toString());
   }
