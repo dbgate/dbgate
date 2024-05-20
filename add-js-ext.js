24904a3b33538e1d7039e27dc13ed0aa65430ed9
Jan Prochazka <jan.prochazka@gmail.com>
2024-05-20 16:36:04 +0200

    add js ext

---
+++ add-js-ext.js
@@ -0,0 +1,16 @@
+module.exports = function (fileInfo, api) {
+  const j = api.jscodeshift;
+  const root = j(fileInfo.source);
+
+  // Find all import declarations
+  root.find(j.ImportDeclaration).forEach(path => {
+    const importPath = path.node.source.value;
+
+    // If the import path doesn't end with .js, add it
+    if (!importPath.endsWith('.js') && !importPath.startsWith('.')) {
+      path.node.source = j.literal(importPath + '.js');
+    }
+  });
+
+  return root.toSource();
+};
