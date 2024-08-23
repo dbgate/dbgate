a13ca9f96a2db500756dfde7e5dd81934b16e647
Jan Prochazka <jan.prochazka@gmail.com>
2024-08-23 09:14:11 +0200

    new icon

--- misc/convert-icons.sh
+++ misc/convert-icons.sh
@@ -35,38 +35,40 @@ RIGHT=850
 
 
 
-magick  \
-    \( \
-        -size 1000x1000 -define gradient:direction=east 'gradient:#0050b3-#1890ff' \
-        \( +clone -fill Black -colorize 100 \
-        -fill White -stroke White -draw "arc $LEFT,750 $RIGHT,950 0,360" -draw "rectangle $LEFT,150 $RIGHT,850" \
-        \) \
-        -alpha off \
-        -compose CopyOpacity -composite \
-    \) \
-    \( \
-        -size 1000x1000 -define gradient:direction=east 'gradient:#096dd9-#40a9ff' \
-        \( +clone -fill Black -colorize 100 \
-        -fill White -draw "arc $LEFT,50 $RIGHT,250 0,360" \
-        \) \
-        -alpha off \
-        -compose CopyOpacity -composite \
-    \) \
-    -compose Over -composite \
-    -strokewidth $STROKE_WIDTH -stroke '#0050b3' -fill transparent \
-    -draw "arc $LEFT,225 $RIGHT,425  0,180" \
-    -draw "arc $LEFT,400 $RIGHT,600  0,180" \
-    -draw "arc $LEFT,575 $RIGHT,775  0,180" \
-    -draw "arc $LEFT,750 $RIGHT,950  0,180" \
-    -draw "arc $LEFT,50 $RIGHT,250  0,360" \
-    -draw "line $LEFT,150 $LEFT,850" \
-    -draw "line $RIGHT,150 $RIGHT,850" \
-    -fill '#fafafa' -stroke '#8c8c8c' -strokewidth 3 \
-    -pointsize 800 -font './Mcbungus-Regular.ttf' \
-    -gravity center \
-    -draw 'text 0,100 "G"' \
-    icon.png
+# magick  \
+#     \( \
+#         -size 1000x1000 -define gradient:direction=east 'gradient:#0050b3-#1890ff' \
+#         \( +clone -fill Black -colorize 100 \
+#         -fill White -stroke White -draw "arc $LEFT,750 $RIGHT,950 0,360" -draw "rectangle $LEFT,150 $RIGHT,850" \
+#         \) \
+#         -alpha off \
+#         -compose CopyOpacity -composite \
+#     \) \
+#     \( \
+#         -size 1000x1000 -define gradient:direction=east 'gradient:#096dd9-#40a9ff' \
+#         \( +clone -fill Black -colorize 100 \
+#         -fill White -draw "arc $LEFT,50 $RIGHT,250 0,360" \
+#         \) \
+#         -alpha off \
+#         -compose CopyOpacity -composite \
+#     \) \
+#     -compose Over -composite \
+#     -strokewidth $STROKE_WIDTH -stroke '#0050b3' -fill transparent \
+#     -draw "arc $LEFT,225 $RIGHT,425  0,180" \
+#     -draw "arc $LEFT,400 $RIGHT,600  0,180" \
+#     -draw "arc $LEFT,575 $RIGHT,775  0,180" \
+#     -draw "arc $LEFT,750 $RIGHT,950  0,180" \
+#     -draw "arc $LEFT,50 $RIGHT,250  0,360" \
+#     -draw "line $LEFT,150 $LEFT,850" \
+#     -draw "line $RIGHT,150 $RIGHT,850" \
+#     -fill '#fafafa' -stroke '#8c8c8c' -strokewidth 3 \
+#     -pointsize 800 -font './Mcbungus-Regular.ttf' \
+#     -gravity center \
+#     -draw 'text 0,100 "G"' \
+#     icon.png
 
+convert icon-input.png -background white -alpha remove -alpha off icon.png 
+convert -size 1000x1000 xc:none -fill white -draw "circle 500,500 500,0" icon.png -compose SrcIn -composite icon.png
 
 # magick \
 #   \( \
@@ -106,4 +108,6 @@ magick icon.png -resize 512x512! ../packages/web/public/logo512.png
 magick icon.png -define icon:auto-resize="256,128,96,64,48,32,16" ../packages/web/public/favicon.ico
 
 convert icon.png -resize 800x800 -background transparent -gravity center -extent 1000x1000 iconmac.png
-magick composite  iconmac.png macbg.png -resize 600x600! ../app/icon512-mac.png
+
+convert macbg.png icon.png -compose SrcIn -composite -resize 600x600! ../app/icon512-mac.png
+# magick composite  iconmac.png macbg.png -resize 600x600! ../app/icon512-mac.png
