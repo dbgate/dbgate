36a65ea13a54aa506f323f7375c4547e56668600
Jan Prochazka <jan.prochazka@sprinx.cz>
2024-09-11 09:34:26 +0200

    mysql - engine

--- plugins/dbgate-plugin-mysql/src/backend/sql/tables.js
+++ plugins/dbgate-plugin-mysql/src/backend/sql/tables.js
@@ -2,6 +2,7 @@ module.exports = `
 select 
 	TABLE_NAME as pureName, 
 	TABLE_ROWS as tableRowCount,
+	ENGINE as tableEngine,
 	case when ENGINE='InnoDB' then CREATE_TIME else coalesce(UPDATE_TIME, CREATE_TIME) end as modifyDate 
 from information_schema.tables 
 where TABLE_SCHEMA = '#DATABASE#' and (TABLE_TYPE='BASE TABLE' or TABLE_TYPE='SYSTEM VERSIONED') and TABLE_NAME =OBJECT_ID_CONDITION;
