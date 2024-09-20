02af761bf7cc0d841899e0f5c15cba91d4c12248
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2024-09-20 15:50:16 +0200

    postgre fix

--- plugins/dbgate-plugin-postgres/src/backend/sql/uniqueNames.js
+++ plugins/dbgate-plugin-postgres/src/backend/sql/uniqueNames.js
@@ -1,3 +1,5 @@
 module.exports = `
-    select conname as "constraint_name" from pg_constraint where contype = 'u' and connamespace =SCHEMA_NAME_CONDITION
+    select cnt.conname as "constraint_name" from pg_constraint cnt 
+    inner join pg_namespace c on c.oid = cnt.connamespace
+     where cnt.contype = 'u' and c.nspname =SCHEMA_NAME_CONDITION
 `;
