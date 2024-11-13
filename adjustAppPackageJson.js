9cc4af2b5661fcebf2d5ffc5e5932fd7f14009ec
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2024-11-13 16:10:05 +0100

    build scripts

--- adjustAppPackageJson.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function adjustAppPackageJson(file) {
-  const json = JSON.parse(fs.readFileSync(file, { encoding: 'utf-8' }));
-  json.workspaces = ['../packages/tools', '../packages/types', '../packages/sqltree'];
-  fs.writeFileSync(file, JSON.stringify(json, null, 2), 'utf-8');
-}
-
-adjustAppPackageJson('app/package.json');
