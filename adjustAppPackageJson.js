56a241b7f450bdb5226b1a8827acaee8a4dec912
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2024-11-13 14:52:48 +0100

    try to fix build

--- adjustAppPackageJson.js
+++ adjustAppPackageJson.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function adjustAppPackageJson(file) {
   const json = JSON.parse(fs.readFileSync(file, { encoding: 'utf-8' }));
-  json.workspaces = ['../packages/*'];
+  json.workspaces = ['../packages/tools', '../packages/types', '../packages/sqltree'];
   fs.writeFileSync(file, JSON.stringify(json, null, 2), 'utf-8');
 }
 
