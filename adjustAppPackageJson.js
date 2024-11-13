7c5479157a0b29bf93709d57401db0b27fdb7a1c
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2024-11-13 13:34:35 +0100

    fix

--- adjustAppPackageJson.js
+++ adjustAppPackageJson.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-function adjustFile(file) {
+function adjustRootPackageJson(file) {
   const json = JSON.parse(fs.readFileSync(file, { encoding: 'utf-8' }));
   json.workspaces.push('app');
   fs.writeFileSync(file, JSON.stringify(json, null, 2), 'utf-8');
 }
 
-adjustFile('app/package.json');
+adjustRootPackageJson('package.json');
