b6399c827181566795f0cfc50c777a064be335a6
Jan Prochazka <prochazka@sprinx.cz>
2024-12-03 16:05:23 +0100

    try to fix test

--- plugins/dbgate-plugin-mysql/src/backend/sql/parameters.js
+++ plugins/dbgate-plugin-mysql/src/backend/sql/parameters.js
@@ -11,7 +11,8 @@ SELECT DISTINCT
     p.NUMERIC_SCALE AS numericScale,
     p.DTD_IDENTIFIER AS dataType,
     COALESCE(NULLIF(PARAMETER_MODE, ''), 'RETURN') AS parameterMode,
-    r.ROUTINE_TYPE AS routineType -- Function or Procedure
+    r.ROUTINE_TYPE AS routineType, -- Function or Procedure
+    p.ORDINAL_POSITION AS ordinalPosition
 FROM 
     information_schema.PARAMETERS p
 JOIN 
@@ -20,4 +21,6 @@ ON
     p.SPECIFIC_NAME = r.SPECIFIC_NAME AND r.ROUTINE_SCHEMA = p.SPECIFIC_SCHEMA
 WHERE 
     r.ROUTINE_SCHEMA = '#DATABASE#' AND r.ROUTINE_NAME =OBJECT_ID_CONDITION
+ORDER BY
+    r.ROUTINE_SCHEMA, r.SPECIFIC_NAME, p.ORDINAL_POSITION
 `;
