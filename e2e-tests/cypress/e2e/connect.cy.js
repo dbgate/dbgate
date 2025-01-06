42ad78df58442a8ac7f8dce893e1fcf808ff73a7
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-01-06 09:46:57 +0100

    keyfile - build containers

--- e2e-tests/cypress/e2e/connect.cy.js
+++ e2e-tests/cypress/e2e/connect.cy.js
@@ -23,7 +23,7 @@ describe('Initialization', () => {
     cy.contains('performance_schema');
   });
 
-  it('SSH connection', () => {
+  it('SSH connection - user + password', () => {
     const runOnCI = Cypress.env('runOnCI');
 
     cy.visit('http://localhost:3000');
@@ -39,6 +39,37 @@ describe('Initialization', () => {
     cy.get('[data-testid=ConnectionDriverFields_password]').clear().type('root');
 
     cy.get('[data-testid=ConnectionDriverFields_displayName]').clear().type('test-mysql-ssh-1');
+
+    cy.get('[data-testid=ConnectionTab_tabSshTunnel]').click();
+    cy.get('[data-testid=ConnectionSshTunnelFields_useSshTunnel]').check();
+    cy.get('[data-testid=ConnectionSshTunnelFields_sshLogin]').clear().type('root');
+    cy.get('[data-testid=ConnectionSshTunnelFields_sshPassword]').clear().type('root');
+    if (runOnCI) {
+      cy.get('[data-testid=ConnectionSshTunnelFields_sshHost]').clear().type('mysql-ssh-login');
+    } else {
+      cy.get('[data-testid=ConnectionSshTunnelFields_sshPort]').clear().type('16006');
+    }
+    cy.get('[data-testid=ConnectionTab_buttonSave]').click();
+    cy.get('[data-testid=ConnectionTab_buttonConnect]').click();
+    cy.contains('performance_schema');
+  });
+
+  it('SSH connection - keyfile', () => {
+    const runOnCI = Cypress.env('runOnCI');
+
+    cy.visit('http://localhost:3000');
+    cy.contains('Connections');
+
+    cy.realPress("F1");
+    cy.realType("Close all");
+    cy.realPress("Enter");
+
+    cy.get('[data-testid=ConnectionList_buttonNewConnection]').click();
+    cy.get('[data-testid=ConnectionDriverFields_connectionType]').select('MySQL');
+    cy.get('[data-testid=ConnectionDriverFields_user]').clear().type('root');
+    cy.get('[data-testid=ConnectionDriverFields_password]').clear().type('root');
+
+    cy.get('[data-testid=ConnectionDriverFields_displayName]').clear().type('test-mysql-ssh-2');
 
     cy.get('[data-testid=ConnectionTab_tabSshTunnel]').click();
     cy.get('[data-testid=ConnectionSshTunnelFields_useSshTunnel]').check();
