3179f2d72c91b6842fb8db7dc80280625c6f359d
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-01-08 16:48:16 +0100

    fixed test

--- e2e-tests/cypress/e2e/add-connection.cy.js
+++ e2e-tests/cypress/e2e/add-connection.cy.js
@@ -101,7 +101,8 @@ describe('Add connection', () => {
     const runOnCI = Cypress.env('runOnCI');
 
     cy.visit('http://localhost:3000');
-    // cy.get('[data-testid=ConnectionList_buttonNewConnection]').click();
+    
+    cy.get('[data-testid=ConnectionList_buttonNewConnection]').click();
     cy.get('[data-testid=ConnectionDriverFields_connectionType]').select('MySQL');
     cy.get('[data-testid=ConnectionDriverFields_user]').clear().type('root');
     cy.get('[data-testid=ConnectionDriverFields_password]').clear().type('Pwd2020Db');
