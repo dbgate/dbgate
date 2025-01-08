9692c0c683af28937df00eb2a0da11acb69ba527
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-01-08 16:35:35 +0100

    try to fix DEX OAUTH

--- e2e-tests/cypress/e2e/oauth.cy.js
+++ e2e-tests/cypress/e2e/oauth.cy.js
@@ -2,10 +2,20 @@ describe('OAuth', () => {
   it('OAuth login', () => {
     cy.visit('http://localhost:3000');
 
+    const runOnCI = Cypress.env('runOnCI');
+
     // login on DEX
-    cy.get('#login').clear().type('test@example.com');
-    cy.get('#password').clear().type('test');
-    cy.get('#submit-login').click();
+    if (runOnCI) {
+      cy.origin('http://dex:5556', () => {
+        cy.get('#login').clear().type('test@example.com');
+        cy.get('#password').clear().type('test');
+        cy.get('#submit-login').click();
+      });
+    } else {
+      cy.get('#login').clear().type('test@example.com');
+      cy.get('#password').clear().type('test');
+      cy.get('#submit-login').click();
+    }
 
     // check DbGate connection
     cy.contains('MySql-connection').click();
