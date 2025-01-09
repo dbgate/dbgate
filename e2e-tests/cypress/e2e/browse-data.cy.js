06be55514cb35e963757d89ee6b0e4a530ab07fb
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-01-09 17:07:30 +0100

    raw data test

--- e2e-tests/cypress/e2e/browse-data.cy.js
+++ e2e-tests/cypress/e2e/browse-data.cy.js
@@ -5,21 +5,39 @@ describe('Data browser data', () => {
     cy.contains('MySql-connection').click();
     cy.contains('Chinook').click();
     cy.contains('Album').click();
-    cy.contains('Let There Be Rock');
+    cy.contains('Let There Be Rock').click();
     cy.contains('Rows: 347');
+    cy.realPress(['Control', 'ArrowRight']);
+    cy.contains('Aerosmith');
   });
 
-  it('Filter model', () => {
+  it.only('Filter model', () => {
     cy.visit('http://localhost:3000');
 
     cy.contains('MySql-connection').click();
     cy.contains('Chinook').click();
     cy.testid('SqlObjectList_search').clear().type('album');
+    cy.contains('Tables (1/11)');
     cy.contains('347 rows, InnoDB');
     cy.testid('SqlObjectList_searchMenuDropDown').click();
     cy.contains('Column name').click();
+    cy.contains('Tables (2/11)');
     cy.contains('AlbumId');
     cy.contains('Column name').click();
     cy.contains('AlbumId').should('not.exist');
+    cy.contains('Tables (1/11)');
+  });
+
+  it('Show raw data', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.contains('MySql-connection').click();
+    cy.contains('Chinook').click();
+    cy.contains('Album').rightclick();
+    cy.contains('Open raw data').click();
+    cy.contains('Let There Be Rock').click();
+    cy.contains('Rows: 347').should('not.exist');
+    cy.realPress(['Control', 'ArrowRight']);
+    cy.contains('Aerosmith').should('not.exist');
   });
 });
