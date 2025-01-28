f0812d74d6cccae96bb0e34e63704286330598d2
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-01-28 10:52:56 +0100

    screenshots fix

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -6,6 +6,8 @@ const { exec } = require('child_process');
 
 module.exports = defineConfig({
   e2e: {
+    trashAssetsBeforeRuns: false,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
