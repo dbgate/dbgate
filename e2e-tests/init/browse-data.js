707ccf2d21419fef2ed6629e15d3a631e96eb25c
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-01-28 13:25:33 +0100

    data

--- e2e-tests/init/browse-data.js
+++ e2e-tests/init/browse-data.js
@@ -80,8 +80,8 @@ async function initPostgresDatabase(dbname, inputFile) {
 
 async function run() {
   await initMySqlDatabase('Chinook', path.resolve(path.join(__dirname, '../data/Chinook-mysql.sql')));
-  await initMySqlDatabase('Northwind', path.resolve(path.join(__dirname, '../data/northwind-mysql.sql')));
-  await initMySqlDatabase('Sakila', path.resolve(path.join(__dirname, '../data/sakila-mysql.sql')));
+  // await initMySqlDatabase('Northwind', path.resolve(path.join(__dirname, '../data/northwind-mysql.sql')));
+  // await initMySqlDatabase('Sakila', path.resolve(path.join(__dirname, '../data/sakila-mysql.sql')));
 
   await initPostgresDatabase('Chinook', path.resolve(path.join(__dirname, '../data/Chinook-postgres.sql')));
 }
