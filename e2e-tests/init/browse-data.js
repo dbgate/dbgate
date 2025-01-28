569b3f5e5602f77532ee770b4de4db3795a94cd1
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-01-28 12:48:07 +0100

    nexte E2E test DBS

--- e2e-tests/init/browse-data.js
+++ e2e-tests/init/browse-data.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const dbgateApi = require('dbgate-api');
 dbgateApi.initializeApiEnvironment();
 const dbgatePluginMysql = require('dbgate-plugin-mysql');
@@ -5,7 +6,7 @@ dbgateApi.registerPlugins(dbgatePluginMysql);
 const dbgatePluginPostgres = require('dbgate-plugin-postgres');
 dbgateApi.registerPlugins(dbgatePluginPostgres);
 
-async function run() {
+async function initMySqlDatabase(dbname, inputFile) {
   await dbgateApi.executeQuery({
     connection: {
       server: process.env.SERVER_mysql,
@@ -14,7 +15,7 @@ async function run() {
       port: process.env.PORT_mysql,
       engine: 'mysql@dbgate-plugin-mysql',
     },
-    sql: 'drop database if exists Chinook',
+    sql: `drop database if exists ${dbname}`,
   });
 
   await dbgateApi.executeQuery({
@@ -25,7 +26,7 @@ async function run() {
       port: process.env.PORT_mysql,
       engine: 'mysql@dbgate-plugin-mysql',
     },
-    sql: 'create database Chinook',
+    sql: `create database ${dbname}`,
   });
 
   await dbgateApi.importDatabase({
@@ -34,10 +35,55 @@ async function run() {
       user: process.env.USER_mysql,
       password: process.env.PASSWORD_mysql,
       port: process.env.PORT_mysql,
+      database: dbname,
       engine: 'mysql@dbgate-plugin-mysql',
     },
-    inputFile: '../data/Chinook-mysql.sql',
+    inputFile,
   });
+}
+
+async function initPostgresDatabase(dbname, inputFile) {
+  await dbgateApi.executeQuery({
+    connection: {
+      server: process.env.SERVER_postgres,
+      user: process.env.USER_postgres,
+      password: process.env.PASSWORD_postgres,
+      port: process.env.PORT_postgres,
+      engine: 'postgres@dbgate-plugin-postgres',
+    },
+    sql: `drop database if exists "${dbname}"`,
+  });
+
+  await dbgateApi.executeQuery({
+    connection: {
+      server: process.env.SERVER_postgres,
+      user: process.env.USER_postgres,
+      password: process.env.PASSWORD_postgres,
+      port: process.env.PORT_postgres,
+      engine: 'postgres@dbgate-plugin-postgres',
+    },
+    sql: `create database "${dbname}"`,
+  });
+
+  await dbgateApi.importDatabase({
+    connection: {
+      server: process.env.SERVER_postgres,
+      user: process.env.USER_postgres,
+      password: process.env.PASSWORD_postgres,
+      port: process.env.PORT_postgres,
+      database: dbname,
+      engine: 'postgres@dbgate-plugin-postgres',
+    },
+    inputFile,
+  });
+}
+
+async function run() {
+  await initMySqlDatabase('Chinook', path.resolve(path.join(__dirname, '../data/Chinook-mysql.sql')));
+  await initMySqlDatabase('Northwind', path.resolve(path.join(__dirname, '../data/northwind-mysql.sql')));
+  await initMySqlDatabase('Sakila', path.resolve(path.join(__dirname, '../data/sakila-mysql.sql')));
+
+  await initPostgresDatabase('Chinook', path.resolve(path.join(__dirname, '../data/Chinook-postgres.sql')));
 }
 
 dbgateApi.runScript(run);
