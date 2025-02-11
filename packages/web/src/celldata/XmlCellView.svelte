aa8dfa1c87fbdb3757967b31e0b7d8fe6a6b03d4
Nybkox <nybkox@gmail.com>
2025-02-11 16:51:39 +0100

    feat: add xml preview

---
+++ packages/web/src/celldata/XmlCellView.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import XmlHighlighter from './XmlHighlighter.svelte';
+
+  export let selection;
+</script>
+
+{#each selection as cell}
+  <XmlHighlighter code={cell.value} />
+{/each}
