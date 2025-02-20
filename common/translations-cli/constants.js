b1ba8879227c4244b730b31a607ae62c090f5e1e
Nybkox <nybkox@gmail.com>
2025-02-20 16:19:46 +0100

    feat: separate remove-unused command

--- common/translations-cli/constants.js
+++ common/translations-cli/constants.js
@@ -1,5 +1,14 @@
 const defaultLanguage = 'en-US';
 
+/** @typedef {{ extensions: string[], directories: string[]}} ExtractConfig
+
+/** @type {ExtractConfig} */
+const defaultExtractConfig = {
+  extensions: ['.js', '.ts', '.svelte'],
+  directories: ['app', 'packages/web'],
+};
+
 module.exports = {
   defaultLanguage,
+  defaultExtractConfig,
 };
