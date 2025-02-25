ec02743f83f27813a3cf6a33b3a101ddbeb62d29
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-02-25 12:38:24 +0100

    SYNC:  test fix

--- e2e-tests/e2eTestTools.js
+++ e2e-tests/e2eTestTools.js
@@ -22,8 +22,8 @@ function clearTestingData() {
   if (fs.existsSync(path.join(baseDir, 'connections-e2etests.jsonl'))) {
     fs.unlinkSync(path.join(baseDir, 'connections-e2etests.jsonl'));
   }
-  if (fs.existsSync(path.join(baseDir, 'settings-e2etests.jsonl'))) {
-    fs.unlinkSync(path.join(baseDir, 'settings-e2etests.jsonl'));
+  if (fs.existsSync(path.join(baseDir, 'settings-e2etests.json'))) {
+    fs.unlinkSync(path.join(baseDir, 'settings-e2etests.json'));
   }
   if (fs.existsSync(path.join(baseDir, 'files-e2etests'))) {
     fs.rmdirSync(path.join(baseDir, 'files-e2etests'), { recursive: true });
