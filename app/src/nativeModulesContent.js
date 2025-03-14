3df928caf4d3c8128c51f637913aacdf1bdf8dd4
Jan Prochazka <jan.prochazka@gmail.com>
2025-03-14 15:31:31 +0100

    deleted obsolete file

--- app/src/nativeModulesContent.js
+++
@@ -1,9 +0,0 @@
-
-// this file is generated automatically by script fillNativeModules.js, do not edit it manually
-const content = {};
-
-content['better-sqlite3'] = () => require('better-sqlite3');
-content['oracledb'] = () => require('oracledb');
-
-
-module.exports = content;
