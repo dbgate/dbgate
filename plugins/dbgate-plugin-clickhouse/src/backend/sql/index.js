9a9b18a3efe40e661767b0a30eef38c21b5ce77d
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-04-02 09:48:35 +0200

    clickhouse: load views for users with limited permissions #1076

--- plugins/dbgate-plugin-clickhouse/src/backend/sql/index.js
+++ plugins/dbgate-plugin-clickhouse/src/backend/sql/index.js
@@ -1,11 +1,13 @@
 const columns = require('./columns');
 const tables = require('./tables');
 const views = require('./views');
+const viewsNoDefinition = require('./viewsNoDefinition');
 const tableModifications = require('./tableModifications');
 
 module.exports = {
   columns,
   tables,
   views,
+  viewsNoDefinition,
   tableModifications,
 };
