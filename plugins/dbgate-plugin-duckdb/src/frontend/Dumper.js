a68a1334fcfb5372dc2e471237bda9922eb8791e
Nybkox <nybkox@gmail.com>
2025-04-03 18:58:40 +0200

    feat(duckdb): add rename sql object

--- plugins/dbgate-plugin-duckdb/src/frontend/Dumper.js
+++ plugins/dbgate-plugin-duckdb/src/frontend/Dumper.js
@@ -2,6 +2,10 @@ const { SqlDumper, arrayToHexString } = require('dbgate-tools');
 
 class Dumper extends SqlDumper {
   autoIncrement() {}
+
+  renameSqlObject(obj, newname) {
+    this.putCmd('^alter %k %f ^rename ^to %i', this.getSqlObjectSqlName(obj.objectTypeField), obj, newname);
+  }
 }
 
 module.exports = Dumper;
