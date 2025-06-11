2da4979e59bfc8a8d4ef659779e195336512945b
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-06-11 14:29:59 +0200

    UX fix

--- packages/web/src/widgets/PublicCloudWidget.svelte
+++ packages/web/src/widgets/PublicCloudWidget.svelte
@@ -45,7 +45,7 @@
       <AppObjectList
         list={$publicFiles || []}
         module={publicCloudFileAppObject}
-        groupFunc={data => data.folder || undefined}
+        groupFunc={data => data.folder || "Not defined"}
         {filter}
       />
     </WidgetsInnerContainer>
