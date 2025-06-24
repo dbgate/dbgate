161586db7ea066b459192888fd838f07e1885e9b
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-06-24 17:13:02 +0200

    SYNC:  try to fix test

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:3000',
+    baseUrl: 'http://localhost:3000',
     // trashAssetsBeforeRuns: false,
 
     setupNodeEvents(on, config) {
