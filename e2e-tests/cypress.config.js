8f6341b903feba246c18dbe59293b7163aa728f7
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-06-25 09:01:39 +0200

    SYNC:  removed baseUrl config

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    // baseUrl: 'http://localhost:3000',
     // trashAssetsBeforeRuns: false,
 
     setupNodeEvents(on, config) {
