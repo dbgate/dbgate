71b191e74018e19baa01351970baf2144e2755e6
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-06-25 09:24:02 +0200

    SYNC:  try to fix test

--- e2e-tests/cypress.config.js
+++ e2e-tests/cypress.config.js
@@ -9,6 +9,7 @@ module.exports = defineConfig({
   e2e: {
     // baseUrl: 'http://localhost:3000',
     // trashAssetsBeforeRuns: false,
+    chromeWebSecurity: false,
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
