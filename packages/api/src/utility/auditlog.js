74b78141b438ab7d272a0d558a04af7c89202d52
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-06-27 10:02:30 +0200

    fake method

--- packages/api/src/utility/auditlog.js
+++ packages/api/src/utility/auditlog.js
@@ -1,7 +1,10 @@
 // only in DbGate Premium
 
 async function sendToAuditLog(req, props) {}
+async function logJsonRunnerScript(req, script) {}
+
 
 module.exports = {
   sendToAuditLog,
+  logJsonRunnerScript,
 };
