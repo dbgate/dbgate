fa02b4fd56cda356e835bf4f5e25ffbafad94ffe
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2025-07-31 10:26:04 +0200

    SYNC:  tag fix

--- packages/api/src/main.js
+++ packages/api/src/main.js
@@ -61,7 +61,7 @@ function registerExpressStatic(app, publicDir) {
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer',${process.env.DBGATE_GTM_ID});</script>
+    })(window,document,'script','dataLayer','${process.env.DBGATE_GTM_ID}');</script>
     <!-- End Google Tag Manager -->`
         );
         html = html.replace(
