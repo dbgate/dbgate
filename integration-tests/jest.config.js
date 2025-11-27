a4c4d17381f148bd25bb274f1f5d8dd80c6d7104
Jan Prochazka <prochazka@sprinx.cz>
2025-11-27 14:47:43 +0100

    test reporters

--- integration-tests/jest.config.js
+++ integration-tests/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  reporters: [
+    'summary',        // nice overall summary
+    'github-actions', // adds inline annotations in PRs
+  ],  
 };
--- packages/filterparser/jest.config.js
+++ packages/filterparser/jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['js'],
+  reporters: [
+    'summary',        // nice overall summary
+    'github-actions', // adds inline annotations in PRs
+  ],  
 };
