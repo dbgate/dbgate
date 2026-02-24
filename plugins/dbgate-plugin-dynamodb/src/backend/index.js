885756b259f50b395f1ad3bf9e3f192582f36a04
SPRINX0\prochazka <jan.prochazka@sprinx.com>
2026-02-24 07:57:10 +0100

    removed ADD plugin

--- plugins/dbgate-plugin-dynamodb/src/backend/index.js
+++
@@ -1,9 +0,0 @@
-const drivers = require('./drivers');
-
-module.exports = {
-  packageName: 'dbgate-plugin-dynamodb',
-  drivers,
-  initialize(dbgateEnv) {
-    drivers.initialize(dbgateEnv);
-  },
-};
--- plugins/dbgate-plugin-dynamodb/src/frontend/index.js
+++
@@ -1,6 +0,0 @@
-const drivers = require('./drivers');
-
-module.exports = {
-  packageName: 'dbgate-plugin-dynamodb',
-  drivers,
-};
