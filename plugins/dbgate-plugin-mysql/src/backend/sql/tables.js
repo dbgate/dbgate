545e7b81e12c64832db9628013538e153adfcdcf
Stela Augustinova <augustinovast@gmail.com>
2026-07-27 14:51:40 +0200

    Update table filtering to include SYSTEM VIEW and SYSTEM VERSIONED types

--- plugins/dbgate-plugin-mysql/src/backend/sql/tables.js
+++ plugins/dbgate-plugin-mysql/src/backend/sql/tables.js
@@ -7,5 +7,5 @@ select
 	DATA_LENGTH + INDEX_LENGTH as sizeBytes,
 	case when ENGINE='InnoDB' then CREATE_TIME else coalesce(UPDATE_TIME, CREATE_TIME) end as modifyDate 
 from information_schema.tables 
-where TABLE_SCHEMA = '#DATABASE#' and (TABLE_TYPE='BASE TABLE' or TABLE_TYPE='SYSTEM VERSIONED') and TABLE_NAME =OBJECT_ID_CONDITION;
+where TABLE_SCHEMA = '#DATABASE#' and TABLE_TYPE in ('BASE TABLE', 'SYSTEM VIEW', 'SYSTEM VERSIONED') and TABLE_NAME =OBJECT_ID_CONDITION;
 `;
