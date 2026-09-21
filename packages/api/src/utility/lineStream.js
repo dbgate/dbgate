6d141ea5c3c9dbf92f9c7bfe9135436c0857f1c5
Stela Augustinova <augustinovast@gmail.com>
2026-09-21 13:16:24 +0200

    Refactor LineStream to use StringDecoder for improved encoding handling

--- packages/api/src/utility/lineStream.js
+++ packages/api/src/utility/lineStream.js
@@ -1,7 +1,8 @@
 const stream = require('stream');
+const { StringDecoder } = require('string_decoder');
 
 // Replacement for the deprecated 'byline' package (https://github.com/jahewson/node-byline),
-// originally (C) 2011-2015 John Hewson, MIT licensed. Same behaviour, without deprecated APIs.
+// originally (C) 2011-2015 John Hewson, MIT licensed.
 
 // see: http://www.unicode.org/reports/tr18/#Line_Boundaries
 const LINE_BOUNDARY_REGEX = new RegExp('\\r\\n|[\\n\\v\\f\\r\\x85\\u2028\\u2029]');
@@ -15,7 +16,8 @@ class LineStream extends stream.Transform {
     this.lineBuffer = [];
     this.keepEmptyLines = options.keepEmptyLines || false;
     this.lastChunkEndedWithCR = false;
-    this.chunkEncoding = 'utf8';
+    this.decoder = null;
+    this.decoderEncoding = 'utf8';
 
     // take the source's encoding if we don't have one
     this.on('pipe', src => {
@@ -26,46 +28,69 @@ class LineStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, done) {
-    // decode binary chunks as UTF-8
-    let chunkEncoding = encoding || 'utf8';
-
-    if (Buffer.isBuffer(chunk)) {
-      if (chunkEncoding == 'buffer') {
-        chunkEncoding = 'utf8';
-      }
-      chunk = chunk.toString(chunkEncoding);
-    }
-    this.chunkEncoding = chunkEncoding;
-
-    const lines = chunk.split(LINE_BOUNDARY_REGEX);
-
-    // don't split CRLF which spans chunks
-    if (this.lastChunkEndedWithCR && chunk[0] == '\n') {
-      lines.shift();
-    }
-
-    if (this.lineBuffer.length > 0) {
-      this.lineBuffer[this.lineBuffer.length - 1] += lines.shift();
-    }
-
-    this.lastChunkEndedWithCR = chunk[chunk.length - 1] == '\r';
-    this.lineBuffer = this.lineBuffer.concat(lines);
-    this._pushBuffer(chunkEncoding, 1, done);
+    this._appendText(this._decode(chunk, encoding));
+    this._pushBuffer(1, done);
   }
 
   _flush(done) {
-    this._pushBuffer(this.chunkEncoding, 0, done);
+    // emit bytes of the last character, if it remained incomplete
+    if (this.decoder) {
+      this._appendText(this.decoder.end());
+    }
+    this._pushBuffer(0, done);
   }
 
-  _pushBuffer(encoding, keep, done) {
+  /**
+   * Decodes binary chunk into string. Decoder keeps its state between chunks, so that
+   * a multi-byte character splitted between two chunks is not decoded as replacement characters.
+   */
+  _decode(chunk, encoding) {
+    if (!Buffer.isBuffer(chunk)) {
+      // encoded source stream gives us strings, no decoding is needed
+      return chunk;
+    }
+
+    const decoderEncoding = !encoding || encoding == 'buffer' ? 'utf8' : encoding;
+    if (!this.decoder || this.decoderEncoding != decoderEncoding) {
+      // encoding should not change during the stream, but if it does, don't lose buffered bytes
+      const rest = this.decoder ? this.decoder.end() : '';
+      this.decoder = new StringDecoder(decoderEncoding);
+      this.decoderEncoding = decoderEncoding;
+      return rest + this.decoder.write(chunk);
+    }
+    return this.decoder.write(chunk);
+  }
+
+  _appendText(text) {
+    if (text.length == 0) {
+      // whole chunk is held by the decoder as a beginning of a splitted character
+      return;
+    }
+
+    const lines = text.split(LINE_BOUNDARY_REGEX);
+
+    // don't split CRLF which spans chunks
+    if (this.lastChunkEndedWithCR && text[0] == '\n') {
+      lines.shift();
+    }
+
+    if (this.lineBuffer.length > 0 && lines.length > 0) {
+      this.lineBuffer[this.lineBuffer.length - 1] += lines.shift();
+    }
+
+    this.lastChunkEndedWithCR = text[text.length - 1] == '\r';
+    this.lineBuffer = this.lineBuffer.concat(lines);
+  }
+
+  _pushBuffer(keep, done) {
     // always buffer the last (possibly partial) line
     while (this.lineBuffer.length > keep) {
       const line = this.lineBuffer.shift();
       // skip empty lines
       if (this.keepEmptyLines || line.length > 0) {
-        if (!this.push(this._reencode(line, encoding))) {
+        if (!this.push(this._encodeLine(line))) {
           // when the high-water mark is reached, defer pushes until the next tick
-          setImmediate(() => this._pushBuffer(encoding, keep, done));
+          setImmediate(() => this._pushBuffer(keep, done));
           return;
         }
       }
@@ -74,15 +99,13 @@ class LineStream extends stream.Transform {
   }
 
   // see Readable::push
-  _reencode(line, chunkEncoding) {
-    if (this.encoding && this.encoding != chunkEncoding) {
-      return Buffer.from(line, chunkEncoding).toString(this.encoding);
-    }
+  _encodeLine(line) {
     if (this.encoding) {
-      // this should be the most common case, i.e. we're using an encoded source stream
+      // source stream is encoded, so it emits strings and we emit strings as well
       return line;
     }
-    return Buffer.from(line, chunkEncoding);
+    // source stream is binary, so we emit buffers encoded the same way as the source
+    return Buffer.from(line, this.decoderEncoding);
   }
 }
 
